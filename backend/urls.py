9155f9812082e47c8182ffb177500992de91ba1f
LearnerBoy443 <cabhirup69@gmail.com>
2026-04-21 10:02:20 +0530

    Delete backend directory

--- backend/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-from api.views import render_index
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', render_index, name='index'),
-    path('api/', include('api.urls')),
-]
